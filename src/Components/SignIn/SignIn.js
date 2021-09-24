5ab132cda6b7fa9da2773093e71bf391bebbcf7e
Kabilan Sakthivelu <kabilansakthi97@gmail.com>
2021-09-24 22:29:38 +0530

    Added contents for SignIn page

--- src/Components/SignIn/SignIn.js
+++ src/Components/SignIn/SignIn.js
@@ -1,10 +1,62 @@
 import React from 'react';
+import {FcGoogle} from 'react-icons/fc';
+import {AiOutlineMail, AiOutlineLock} from 'react-icons/ai';
+import {Link} from 'react-router-dom';
 import './SignIn.css';
 
 const SignIn = () => {
+
+    const signin = (e) =>{
+        e.preventDefault();
+        console.log("hello all");
+    }
+
     return (
-        <div>
-            <h1>SignIn</h1>
+        <div className="signInPage">
+
+        {/* Sign In page Left panel */}
+
+        <div className="signInLeftPanel">
+        </div>
+
+        {/* Sign In page right panel */}
+
+        <div className="signInModal">
+            <h1 className="title">Food Dunzo</h1>
+            <p className="description">One stop for all your food needs</p>
+            <div className="googleSignInBtn">
+            <FcGoogle className="googleIcon"/>
+            <p className="googleSignInBtnDesc">Continue with Google</p>
+            </div>
+
+            <div className="orDividerDiv">
+            <hr className="orDividerRuler"/>
+            <p className="orDivider">or</p>
+            <hr className="orDividerRuler"/>
+            </div>
+
+            {/* Login Form */}
+
+            <form className="loginForm" onSubmit={signin}>
+
+            <div className="inputFieldDiv">
+            <AiOutlineMail className="inputFieldIcon"/>
+            <input className="inputField" required type="email" name="email" id="email" placeholder="Email" />
+            </div>
+
+            <div className="inputFieldDiv">
+            <AiOutlineLock className="inputFieldIcon"/>
+            <input className="inputField" required type="password" name="password" id="password" placeholder="Password"/>
+            </div>
+
+            <button className="signInBtn">Sign In</button>
+            </form>
+
+            <div className="footer">
+            <p>Don't have an account?</p>
+            <Link to="/signup" className="signUpLink">Register</Link>
+            </div>
+        </div>
         </div>
     )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+         'image': "url('https://a-static.besthdwallpaper.com/western-food-hamburger-french-fries-and-variety-of-vegetable-in-one-table-wallpaper-3840x2400-60595_9.jpg')",
+        }
+    },
   },
   variants: {
     extend: {},
